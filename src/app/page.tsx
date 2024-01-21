a4682f4aa70f222fafe91ed5aeb534e33dd41857
Meekunn <orofinadedamola@gmail.com>
2024-01-21 07:57:13 +0100

    resolve build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,22 @@ import HomeSection from "@components/Home";
 import TheCouple from "@components/TheCouple";
 import Proposal from "@components/Proposal";
 import OrderOfProgram from "@components/OrderOfProgram";
-// import Gifts from "@components/Gifts";
 import Contact from "@components/Contact";
 import Footer from "@components/Footer";
+import Gifts from "@components/Gifts";
 
 export default function Home() {
-	return (
-		<>
-			{/* <Navbar /> */}
-			<HomeSection />
-			<TheCouple />
-			<Proposal />
-			<OrderOfProgram />
-			{/* <Details /> */}
-			{/* <Gifts /> */}
-			<Contact />
-			<Footer />
-		</>
-	);
+    return (
+        <>
+            {/* <Navbar /> */}
+            <HomeSection />
+            <TheCouple />
+            <Proposal />
+            <OrderOfProgram />
+            {/* <Details /> */}
+            <Gifts />
+            <Contact />
+            <Footer />
+        </>
+    );
 }
